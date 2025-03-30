9374da055aa940fc671075f206acdd5d00392ded
abdout <osmanabdout@hotmail.com>
2025-03-30 22:53:52 +0300

    x

--- apps/www/app/(app)/blocks/page.tsx
+++ apps/www/app/(app)/blocks/page.tsx
@@ -74,7 +74,7 @@ export default function BlocksPage() {
   return (
    
     
-        <div className="container mx-auto grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-12">
+        <div className="mx-auto container grid grid-cols-1 gap-4 py-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {blocks.map((block) => (
             <Link
               key={block.id}
