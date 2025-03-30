274af083503625de2a7c6c7c448da3a3aa1c765f
abdout <osmanabdout@hotmail.com>
2025-03-30 17:44:39 +0300

    x

--- apps/www/app/(app)/micros/page.tsx
+++ apps/www/app/(app)/micros/page.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Micros</div>
+    <div>
+      <h1>Micros</h1>
+      <p>
+        Micros are small, reusable components that can be used to build
+        complex UIs.
+      </p>
+      <p>
+        They are designed to be used in conjunction with the Block component,
+        which is a container for micros.
+      </p>
+    </div>
   )
 }
 
