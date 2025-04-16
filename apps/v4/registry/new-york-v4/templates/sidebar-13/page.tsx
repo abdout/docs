f44ea652cdf994c09dd865a683b0720ba5847222
abdout <osmanabdout@hotmail.com>
2025-04-16 19:56:14 +0300

    pure docs

--- apps/v4/registry/new-york-v4/templates/sidebar-13/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SettingsDialog } from "@/registry/new-york-v4/templates/sidebar-13/components/settings-dialog"
-
-export default function Page() {
-  return (
-    <div className="flex h-svh items-center justify-center">
-      <SettingsDialog />
-    </div>
-  )
-}
